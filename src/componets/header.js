887f2d3660efbadf26905fcbd8c97375ddf50219
Desmond Kinoti <desmond.kinoti@student.moringaschool.com>
2023-06-11 19:48:44 +0100

    update

---
+++ src/componets/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './header.css';
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+    <header className="header">
+        <a href="https://saigowthamr.github.io/Itunes-Top-100-albums/" style={{
+            textDecoration: "none",
+            color: "white", borderBottom: "1px solid red"
+        }}>Top 100 Albums from Itunes</a>
+    </header>
+)
