1a48b451d083094e9a87c668c187be0c6cdf8462
Desmond Kinoti <desmond.kinoti@student.moringaschool.com>
2023-06-11 19:14:51 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 
-import './App.css';
+import React, { Suspense, lazy, Component } from 'react';
+import axios from "axios";
+import Header from './components/header';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        </header>
+const Albums = lazy(() => import('./components/albums'));
+
+
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://itunes.apple.com/us/rss/topalbums/limit=100/json`)
+      .then(res => {
+        this.setState({ posts: res.data.feed.entry });
+      });
+  }
+
+  render() {
+    const loadingImg = <div className="album-img">
+      <img alt="loading" src="https://media.giphy.com/media/y1ZBcOGOOtlpC/200.gif" />
     </div>
-  );
+
+    const albums = this.state.posts.map(e => {
+      return (
+        <Suspense key={e.id.label} fallback={loadingImg}>
+          <Albums
+            image={e["im:image"][2].label}
+            title={e.title.label}
+            link={e.id.label}
+            price={e["im:price"].label}
+            date={e["im:releaseDate"].label}
+          />
+        </Suspense>
+      );
+    });
+
+    return (
+      <div className="app">
+        <Header />
+        <div className="albums">
+          {albums}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
